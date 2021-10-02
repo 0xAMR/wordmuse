d8f4409597a045b567bf4c4bfeefc19ee8a17d90
andreiracasan <69733376+AndreiRacasan@users.noreply.github.com>
2021-10-02 17:56:10 +0100

    💄 improve UI

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,17 @@ const StyledApp = styled.main`
   align-items: center;
   justify-content: center;
   min-height: 100vh;
-  background-color: #fff;
+
+  & .main__layout {
+    background-color: rgba(252, 252, 252);
+
+    & .main__footer {
+      background-color: transparent;
+      color: #888;
+      font-size: 0.95em;
+      text-align: center;
+    }
+  }
 
   & .main__header {
     display: flex;
@@ -22,20 +32,25 @@ const StyledApp = styled.main`
 
     & .main__logo {
       color: white;
-      font-size: 1.5em;
+      font-size: 1.75em;
       font-weight: 600;
     }
   }
 
   & .main__title {
+    color: #333;
     padding: 0;
     margin: 0;
   }
 
   & .main__subtitle {
+    color: #666;
+    font-style: italic;
+    text-transform: capitalize;
+    font-weight: 500;
     padding: 0;
     margin-top: 1em;
-    margin-bottom: 1.5em;
+    margin-bottom: 1.75em;
   }
 
   & .main__content {
@@ -43,7 +58,7 @@ const StyledApp = styled.main`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin-bottom: 10rem;
+    margin-bottom: 7.5rem;
   }
 
   & .layout {
@@ -54,11 +69,10 @@ const StyledApp = styled.main`
 function App() {
   return (
     <StyledApp>
-      <Layout className="layout">
+      <Layout className="layout main__layout">
         <Header className="main__header">
           <div className="main__logo">Wordmuse</div>
           <Menu theme="dark" mode="horizontal">
-            <Menu.Item>About</Menu.Item>
             <Menu.Item>Developer</Menu.Item>
             <Menu.Item>API</Menu.Item>
           </Menu>
@@ -66,7 +80,7 @@ function App() {
         <Content className="main__content">
           <Title className="main__title">Wordmuse</Title>
           <Title className="main__subtitle" level={5}>
-            World's favourite word query engine
+            world's favourite word query engine
           </Title>
           <Search
             style={{ width: 500 }}
@@ -76,7 +90,7 @@ function App() {
             enterButton
           />
         </Content>
-        <Footer style={{ textAlign: 'center' }}>© Wordmuse 2021</Footer>
+        <Footer className="main__footer">© Wordmuse 2021</Footer>
       </Layout>
     </StyledApp>
   );
