af90cafea410b05992895665cef1c251048398a0
andreiracasan <69733376+AndreiRacasan@users.noreply.github.com>
2021-10-02 14:40:57 +0100

    💄 build basic UI with ant

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-// Material UI
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-
 // Styling
 import styled from 'styled-components';
+
+// Ant Design
+import { Input, Layout, Menu, Typography } from 'antd';
+const { Search } = Input;
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 
 const StyledApp = styled.main`
   display: flex;
@@ -11,23 +13,71 @@ const StyledApp = styled.main`
   align-items: center;
   justify-content: center;
   min-height: 100vh;
-  background-color: #001e3c;
+  background-color: #fff;
+
+  & .main__header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    & .main__logo {
+      color: white;
+      font-size: 1.5em;
+      font-weight: 600;
+    }
+  }
+
+  & .main__title {
+    padding: 0;
+    margin: 0;
+  }
+
+  & .main__subtitle {
+    padding: 0;
+    margin-top: 1em;
+    margin-bottom: 1.5em;
+  }
+
+  & .main__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 10rem;
+  }
+
+  & .layout {
+    width: 100%;
+  }
 `;
 
 function App() {
   return (
     <StyledApp>
-
-      <Box>
-      <TextField
-      style={{backgroundColor: 'rgba(255,255,255, 0.15)'}}
-          label="Size"
-          id="filled-size-normal"
-          defaultValue="Normal"
-          variant="filled"
-        />
-      </Box>
-        
+      <Layout className="layout">
+        <Header className="main__header">
+          <div className="main__logo">Wordmuse</div>
+          <Menu theme="dark" mode="horizontal">
+            <Menu.Item>About</Menu.Item>
+            <Menu.Item>Developer</Menu.Item>
+            <Menu.Item>API</Menu.Item>
+          </Menu>
+        </Header>
+        <Content className="main__content">
+          <Title className="main__title">Wordmuse</Title>
+          <Title className="main__subtitle" level={5}>
+            World's favourite word query engine
+          </Title>
+          <Search
+            style={{ width: 500 }}
+            placeholder="Enter word"
+            size="large"
+            onSearch={() => {}}
+            enterButton
+          />
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>© Wordmuse 2021</Footer>
+      </Layout>
     </StyledApp>
   );
 }
