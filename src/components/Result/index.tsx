7a37b64f347ba5423d567a9e51426440066d44b2
andreiracasan <69733376+AndreiRacasan@users.noreply.github.com>
2021-10-31 21:02:00 +0000

    💄 ♻️ refactor result map and minor UI changes

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -1,21 +1,31 @@
+// Styling
+import styled from 'styled-components';
+
 // Ant Design
 import { Divider, Layout, Typography } from 'antd';
 const { Content } = Layout;
 const { Title } = Typography;
 
+const StyledResult = styled.div`
+  & .result__content {
+    margin: 1em 1.5em;
+    padding: 0.25em;
+  }
+`;
+
 type ResultsProps = {
   title: string;
-  description: string;
 };
 
-export default function Result({ title, description }: ResultsProps) {
+export default function Result({ title }: ResultsProps) {
   return (
-    <Content>
-      <Title className="main__title">{title}</Title>
-      <Title className="main__subtitle" level={5}>
-        {description}
-      </Title>
-      <Divider />
-    </Content>
+    <StyledResult>
+      <Content className="result__content">
+        <Title className="result__title" level={4}>
+          {title}
+        </Title>
+        <Divider />
+      </Content>
+    </StyledResult>
   );
 }
--- src/components/ResultsList/index.tsx
+++ src/components/ResultsList/index.tsx
@@ -1,4 +1,23 @@
+// Styling
+import styled from 'styled-components';
+
+// Components
 import Result from '../Result';
+
+// Ant Design
+import { Typography } from 'antd';
+const { Title } = Typography;
+
+const StyledList = styled.div`
+  width: 100%;
+  max-width: 750px;
+  margin: 0 auto;
+
+  & .list__title {
+    padding: 2em 0;
+    font-size: 1.75em;
+  }
+`;
 
 type ResultsListProps = {
   data: object;
@@ -6,10 +25,13 @@ type ResultsListProps = {
 
 export default function ResultsList({ data }: ResultsListProps): JSX.Element {
   return (
-    <>
-      {Object.keys(data).map((_word) => (
-        <Result title={_word} description="testing" />
+    <StyledList>
+      <Title className="list__title" level={3}>
+        Search results pertaining to $param
+      </Title>
+      {Object.values(data).map((result) => (
+        <Result title={result.word} />
       ))}
-    </>
+    </StyledList>
   );
 }
