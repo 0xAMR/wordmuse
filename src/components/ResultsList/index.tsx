2a16c854c318c0d0cfc5c472bba3c9e6bb8a17ec
andreiracasan <69733376+AndreiRacasan@users.noreply.github.com>
2021-11-04 20:57:37 +0000

    ✨ (i) map through and display results & (ii) return home

--- src/components/ResultsList/index.tsx
+++ src/components/ResultsList/index.tsx
@@ -5,12 +5,14 @@ import styled from 'styled-components';
 import Result from '../Result';
 
 // Ant Design
-import { Typography } from 'antd';
+import { Typography, Button } from 'antd';
+import { ArrowLeftOutlined } from '@ant-design/icons';
 const { Title } = Typography;
 
 const StyledList = styled.div`
   width: 100%;
   max-width: 750px;
+  padding: 2em 1em;
   margin: 0 auto;
 
   & .list__title {
@@ -21,13 +23,27 @@ const StyledList = styled.div`
 
 type ResultsListProps = {
   data: object;
+  wordInput: string;
+  backHome: () => void;
 };
 
-export default function ResultsList({ data }: ResultsListProps): JSX.Element {
+export default function ResultsList({
+  data,
+  wordInput,
+  backHome,
+}: ResultsListProps): JSX.Element {
   return (
     <StyledList>
+      <Button
+        onClick={backHome}
+        type="primary"
+        icon={<ArrowLeftOutlined />}
+        size="large"
+      >
+        Return Home
+      </Button>
       <Title className="list__title" level={3}>
-        Search results pertaining to $param
+        Search results pertaining to '{wordInput}'
       </Title>
       {Object.values(data).map((result) => (
         <Result title={result.word} />
