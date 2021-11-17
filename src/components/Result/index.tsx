f16a8eade961bfbf80d582b4c561d433fbd27f5d
andreiracasan <69733376+AndreiRacasan@users.noreply.github.com>
2021-11-17 21:19:51 +0000

    💄 minor UI fixes

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -6,17 +6,28 @@ import { Layout, Typography } from 'antd';
 const { Content } = Layout;
 const { Title } = Typography;
 
-const StyledResult = styled.div`
+const StyledResult = styled.button`
+  border: 0;
+  background: transparent;
+  cursor: pointer;
+  margin: 1em 1.5em;
+  border: 1px solid #d9d9d9;
+  border-radius: 2px;
+  transition: transform 200ms ease-in-out;
+
+  &:hover {
+    background: #f0f0f0;
+    border: 1px solid #ccc;
+    transform: scale(1.05);
+  }
+
   & .result__content {
     display: flex;
     align-items: center;
     justify-content: center;
-    margin: 1em 1.5em;
     padding: 0.25em;
     width: 175px;
     height: 60px;
-    border: 1px solid #d9d9d9;
-    border-radius: 2px;
 
     & .result__title {
       margin: 0 0 0.25em 0;
