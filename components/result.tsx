20a879bfd22b1ca8e802e9282f77a8714bb0a8fc
andreiracasan <69733376+AndreiRacasan@users.noreply.github.com>
2022-02-14 23:11:07 +0000

    ✨ results list

---
+++ components/result.tsx
@@ -0,0 +1,57 @@
+// Styling
+import styled from 'styled-components';
+
+// Ant Design
+import { Layout, Typography } from 'antd';
+const { Content } = Layout;
+const { Title } = Typography;
+
+const StyledResult = styled.button`
+  border: 0;
+  background: transparent;
+  cursor: pointer;
+  margin: 1em 1.5em;
+  border: 1px solid #d9d9d9;
+  border-radius: 2px;
+  transition: transform 200ms ease-in-out;
+
+  &:hover {
+    background: #f0f0f0;
+    border: 1px solid #ccc;
+    transform: scale(1.05);
+  }
+
+  & .result__content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.25em;
+    width: 175px;
+    height: 60px;
+
+    & .result__title {
+      margin: 0 0 0.25em 0;
+      font-weight: 500;
+    }
+  }
+`;
+
+type ResultsProps = {
+  title: string;
+  onClick: () => void;
+};
+
+export default function Result({ title, onClick }: ResultsProps) {
+  return (
+    <StyledResult
+      onClick={onClick}
+      title={`Click for a full definition of ${title}`}
+    >
+      <Content className="result__content">
+        <Title className="result__title" level={4}>
+          {title}
+        </Title>
+      </Content>
+    </StyledResult>
+  );
+}
--- pages/search/[id].tsx
+++ pages/search/[id].tsx
@@ -1,8 +1,120 @@
+// Next
 import { useRouter } from 'next/router';
 
-export default function Search() {
+// React
+import { useEffect, useState } from 'react';
+
+// Styling
+import styled from 'styled-components';
+
+// Components
+import Result from '../../components/result';
+
+// Ant Design
+import { Typography, Button } from 'antd';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+const { Title } = Typography;
+
+const StyledList = styled.div`
+  width: 100%;
+  max-width: 750px;
+  padding: 2em 1em;
+  margin: 0 auto;
+
+  & .list__title {
+    padding: 2em 0 0.25em 0;
+    font-size: 1.75em;
+  }
+
+  & .list__container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+// check if object is empty
+const isEmpty = (obj: any) => {
+  for (const key in obj) return false;
+
+  return true;
+};
+
+type ResultsListProps = {
+  wordInput: string;
+  wordType: string;
+  backHome: () => void;
+};
+
+export default function ResultsList({
+  wordInput,
+  wordType,
+  backHome,
+}: ResultsListProps): JSX.Element {
+  const [searchResults, setSearchResults] = useState({});
+
   const router = useRouter();
   const { id } = router.query;
 
-  return <h1>{id}</h1>;
+  const searchCriteria: Record<string, string> = {
+    rel_rhy: 'rhyme with',
+    sl: 'sound like',
+    sp: 'are spelled like',
+    ml: 'are related to',
+    rel_ant: 'are antonyms of',
+  };
+
+  /**
+   * take word, get data from api
+   *
+   * @param {string} word input string
+   * @returns words that match the input string
+   */
+  async function getData(word: string | string[] | undefined) {
+    const response = await fetch(
+      `https://api.datamuse.com/words?${wordType}=${word}`
+    );
+    const data = await response.json();
+
+    return data;
+  }
+
+  useEffect(() => {
+    const _word = id;
+
+    getData(_word).then((data) => {
+      setSearchResults(data);
+    });
+  }, [getData, id]);
+
+  return (
+    <StyledList>
+      <Button
+        onClick={() => {}}
+        type="primary"
+        icon={<ArrowLeftOutlined />}
+        size="large"
+      >
+        Back
+      </Button>
+
+      {isEmpty({}) ? (
+        <Title className="list__title" level={3}>
+          Sorry, we could not find any results for your search. Please try
+          searching for a different word. {id}
+        </Title>
+      ) : (
+        <Title className="list__title" level={3}>
+          Search results pertaining to words that...
+        </Title>
+      )}
+
+      <div className="list__container">
+        {Object.values(searchResults).map((result: any) => (
+          <Result key={result.word} onClick={() => {}} title={result.word} />
+        ))}
+      </div>
+    </StyledList>
+  );
 }
